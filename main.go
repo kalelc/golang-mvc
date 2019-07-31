34fe8e2069e5305c1a5dd92494422bc9c968f293
Andres Colonia <andreskal@gmail.com>
2019-07-30 22:06:57 -0400

    refactor: include package view using view struct to contact and home

--- main.go
+++ main.go
@@ -1,32 +1,39 @@
 package main
 
 import (
-	"golang-mvc/views"
-	"html/template"
+	"fmt"
 	"net/http"
+
+	"github.com/kalelc/golang-mvc/views"
 
 	"github.com/gorilla/mux"
 )
 
 var homeView *views.View
+var contactView *views.View
 
 func main() {
-	var err error
-	homeTemplate, err = template.ParseFiles(
-		"views/home.gohtml",
-		"views/layouts/top.gohtml")
-	if err != nil {
-		panic(err)
-	}
+	homeView = views.NewView("views/home.gohtml")
+	contactView = views.NewView("views/contact.gohtml")
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+
+	fmt.Println("localhost:3000")
 	http.ListenAndServe(":3000", r)
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if err := homeTemplate.Execute(w, nil); err != nil {
+	if err := homeView.Template.Execute(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	if err := contactView.Template.Execute(w, nil); err != nil {
 		panic(err)
 	}
 }
---
+++ views/contact.gohtml
@@ -0,0 +1,1 @@
+<h1>Contact View</h1>
