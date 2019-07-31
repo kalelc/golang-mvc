dc0701fc01dd04af891b0c48787f573e542e3391
Andres Colonia <andreskal@gmail.com>
2019-07-30 22:35:46 -0400

    refactor: include several layouts

--- views/view.go
+++ views/view.go
@@ -1,9 +1,14 @@
 package views
 
-import "html/template"
+import (
+	"fmt"
+	"html/template"
+)
 
 func NewView(files ...string) *View {
-	files = append(files, "views/layouts/top.gohtml")
+	files = append(files, "views/layouts/top.html", "views/layouts/footer.html")
+
+	fmt.Println(files)
 
 	t, err := template.ParseFiles(files...)
 	if err != nil {
