ee6333a12a683a081acb53c59be49fbb86e0d9e3
Andres Colonia <andreskal@gmail.com>
2019-08-04 21:28:16 -0400

    refactor: extend view to use render function

--- views/view.go
+++ views/view.go
@@ -2,13 +2,17 @@ package views
 
 import (
 	"html/template"
+	"net/http"
+	"path/filepath"
+)
+
+var (
+	LayoutDir   string = "views/layouts/"
+	TemplateExt string = ".html"
 )
 
 func NewView(layout string, files ...string) *View {
-	files = append(files,
-		"views/layouts/top.html",
-		"views/layouts/footer.html",
-		"views/layouts/bootstrap.html")
+	files = append(files, layoutFiles()...)
 
 	t, err := template.ParseFiles(files...)
 	if err != nil {
@@ -21,7 +25,20 @@ func NewView(layout string, files ...string) *View {
 	}
 }
 
+func layoutFiles() []string {
+	files, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
+	if err != nil {
+		panic(err)
+	}
+	return files
+}
+
 type View struct {
 	Template *template.Template
 	Layout   string
+}
+
+func (v *View) Render(w http.ResponseWriter,
+	data interface{}) error {
+	return v.Template.ExecuteTemplate(w, v.Layout, data)
 }
