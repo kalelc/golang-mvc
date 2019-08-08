a55c178f6b9df1bda4ee7763d772e32a81f0e7b9
Andres Colonia <andreskal@gmail.com>
2019-08-08 18:07:27 -0400

    feat: create users controller

---
+++ controllers/users.go
@@ -0,0 +1,23 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/kalelc/golang-mvc/views"
+)
+
+func NewUsers() *Users {
+	return &Users{
+		NewView: views.NewView("bootstrap", "views/users/new.html"),
+	}
+}
+
+func (u *Users) New(w http.ResponseWriter, r *http.Request) {
+	if err := u.NewView.Render(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+type Users struct {
+	newView *views.View
+}
--- main.go
+++ main.go
@@ -4,7 +4,8 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/kalelc/golang-templates/views"
+	"github.com/kalelc/golang-mvc/controllers"
+	"github.com/kalelc/golang-mvc/views"
 
 	"github.com/gorilla/mux"
 )
@@ -12,18 +13,17 @@ import (
 var (
 	homeView    *views.View
 	contactView *views.View
-	signupView  *views.View
 )
 
 func main() {
 	homeView = views.NewView("bootstrap", "views/home.html")
 	contactView = views.NewView("bootstrap", "views/contact.html")
-	signupView = views.NewView("bootstrap", "views/signup.html")
+	usersC := controllers.NewUsers()
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/signup", signup)
+	r.HandleFunc("/signup", usersC.New)
 
 	fmt.Println("localhost:3000")
 	http.ListenAndServe(":3000", r)
@@ -38,12 +38,6 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 
 	must(contactView.Render(w, nil))
-}
-
-func signup(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-
-	must(signupView.Render(w, nil))
 }
 
 func must(err error) {
