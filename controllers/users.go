225061aab0830cc9c03f57d8029e4214879e62eb
Andres Colonia <andreskal@gmail.com>
2019-08-09 14:53:04 -0400

    refactor: change struct fix schema

--- controllers/users.go
+++ controllers/users.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/gorilla/schema"
 	"github.com/kalelc/golang-mvc/views"
 )
 
@@ -13,26 +14,30 @@ func NewUsers() *Users {
 	}
 }
 
+type Users struct {
+	NewView *views.View
+}
+
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	if err := u.NewView.Render(w, nil); err != nil {
 		panic(err)
 	}
 }
 
-func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
-	var form SignupForm
-	if err := parseForm(r, &form); err != nil {
-		panic(err)
-	}
-	fmt.Fprintln(w, "Email is", form.Email)
-	fmt.Fprintln(w, "Password is", form.Password)
-}
-
-type Users struct {
-	NewView *views.View
-}
-
 type SignupForm struct {
 	Email    string `schema:"email"`
 	Password string `schema:"password"`
+}
+
+func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		panic(err)
+	}
+
+	dec := schema.NewDecoder()
+	form := SignupForm{}
+	if err := dec.Decode(&form, r.PostForm); err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, form)
 }
